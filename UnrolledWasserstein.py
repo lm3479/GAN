a09b81c9e76861c963b14c878c2faf6fcd61be1b
lm3479 <162853856+lm3479@users.noreply.github.com>
2024-05-16 16:58:27 -0600

    Update UnrolledWasserstein.py

--- UnrolledWasserstein.py
+++ UnrolledWasserstein.py
@@ -1,1 +1,2 @@
-
+#Unrolled generative adversarial networks: reduces the probability of mode collapse by playing 'k' number of steps for how the generator can be optimized
+#At each step, gradient descent 
