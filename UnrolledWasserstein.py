5450b6b394d776b6c04771dd5284d1d8309ac23c
lm3479 <162853856+lm3479@users.noreply.github.com>
2024-05-16 16:59:12 -0600

    Update UnrolledWasserstein.py

--- UnrolledWasserstein.py
+++ UnrolledWasserstein.py
@@ -1,2 +1,3 @@
 #Unrolled generative adversarial networks: reduces the probability of mode collapse by playing 'k' number of steps for how the generator can be optimized
-#At each step, gradient descent 
+#At each step, gradient descent is used,
+#To summarize, the Unrolled GAN uses the cost function calculated in the last step to perform the backpropagation for the generator while the discriminator uses the first step only.
