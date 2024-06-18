b559cae7dee4a86cf93e515f1bee621e3f139139
lm3479 <162853856+lm3479@users.noreply.github.com>
2024-06-18 16:40:37 -0600

    Update NovelGAN.py

--- NovelGAN.py
+++ NovelGAN.py
@@ -239,7 +239,54 @@ class WGANGP(object):
             )
             self.gen_trainer.compile(optimizer=self.opt_gen,
                 loss=wasserstein_loss)
-      
+
+def predict_ehull(dir, model_path, output_path, api_key):
+  m3gnet_e_form = M3GNet.from_dir(model_path)
+  ehull_list = []
+  for file_name in os.listdir(dir):
+      crystal = Structure.from_file(dir + file_name, sort = True, merge_tol=0.01)
+      try:
+          e_form_predict = m3gnet_e_form.predict_structure(crystal)
+        
+      except:
+          print("Could not predict formation energy of ", crystal)
+          ehull_list.append((file_name, "N/A"))
+          break
+      elements = ''.join([i for i in crystal.formula if not i.isdigit()]).split(" ")
+      mat_api_key = api_key
+      mpr = MPRester(mat_api_key)
+  #Extracts information about crystals, and additional compounds
+      all_compounds = mpr.summary.search(elements = elements)
+      insert_list = []
+    for compound in all_compounds:
+          for element in ''.join([i for i in str(compound.composition) if not i.isdigit()]).split(" "):
+              if element not in elements and element not in insert_list:
+                insert_list.append(element)
+          for element in elements + insert_list:
+              all_compounds += mpr.summary.search(elements = [element], num_elements = (1,1))
+          pde_list = []
+          for i in range(len(all_compounds)):
+              comp = str(all_compounds[i].composition.reduced_composition).replace(" ", "")
+              pde_list.append(ComputedEntry(comp, all_compounds[i].formation_energy_per_atom))
+          try:
+              diagram = PhaseDiagram(pde_list)
+              _, pmg_ehull = diagram.get_decomp_and_e_above_hull(ComputedEntry(Composition(crystal.formula.replace(" ", "")), e_form_predict[0][0].numpy()))
+              ehull_list.append((file_name, pmg_ehull))
+          except:
+              print("Could not create phase diagram")
+              ehull_list.append((file_name, "N/A"))
+              continue
+      np.save(output_path, np.array(ehull_list))
+  #Predicting distance above convex hull: anything on convex hull is considered stable
+      m3gnet_model = M3GNET.from_dir(args.m3gnet_model_path)
+      stable_ehulls = []
+def filter_unrealistic_structures(m3gnet_model, pmg_ehull, ehull_threshold=0.1):
+  if pmg_ehull <= ehull_threshold:  
+      stable_ehulls.append(pmg_ehull)
+  else:
+      print("Unrealistic structure discarded: high energy above hull.")
+    return False
+  
     def fit_generator(self,noise_gen, dataset, latent_dim, n_epochs = 10, n_batch = 256,n_critic = 5, model_name=None):
         bat_per_epoch = int(53856/n_batch)
         n_steps = bat_per_epoch*n_epochs
